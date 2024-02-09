0611e24ef711aa5f69ec14392dd6442f99f202e1
Scott A <89099102+Scotab@users.noreply.github.com>
2024-02-08 20:53:34 -0500

    do not code while stupid :3

--- Core/Inc/analyzer.h
+++ Core/Inc/analyzer.h
@@ -4,6 +4,7 @@
 //#include <nerduino.h> Replace
 #include "datastructs.h"
 #include "segment.h"
+#include "stm32fxx_hal.h"
 
 /* We want to make sure we aren't doing useless analysis on the same set of data since we are
  * backfilling segment data */
