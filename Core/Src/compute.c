edf9c6de17d570ca13afc585c46094821dcfed4d
Daniel <38150780+dnakhooda@users.noreply.github.com>
2024-11-06 11:09:44 -0500

    bug/MessageFiltering (#130)
    * Add New Filtering
    
    * Fix Formatting
    
    * Remove Can ID List
    
    * bump submodule
    
    ---------
    
    Co-authored-by: Jack Rubacha <rubacha.jack03@gmail.com>

--- Core/Src/compute.c
+++ Core/Src/compute.c
@@ -54,17 +54,23 @@ uint8_t compute_init(acc_data_t *bmsdata)
 {
 	// TODO throw all of these objects into a compute struct
 	can1.hcan = &hcan1;
-	can1.id_list = can1_id_list;
-	can1.id_list_len = sizeof(can1_id_list) / sizeof(can1_id_list[0]);
 	// can1.callback = can_receive_callback;
 	can1_rx_queue = ringbuffer_create(MAX_CAN1_STORAGE, sizeof(can_msg_t));
+
+	uint32_t can1_id_list_size_four[4] = { can1_id_list[0], can1_id_list[0],
+					       can1_id_list[0],
+					       can1_id_list[0] };
+	can_add_filter(&can1, can1_id_list_size_four);
 	can_init(&can1);
 
 	can2.hcan = &hcan2;
-	can2.id_list = can2_id_list;
-	can2.id_list_len = sizeof(can2_id_list) / sizeof(can2_id_list[0]);
 	// can2.callback = can_receive_callback;
 	can2_rx_queue = ringbuffer_create(MAX_CAN2_STORAGE, sizeof(can_msg_t));
+
+	uint32_t can2_id_list_size_four[4] = { can2_id_list[0], can2_id_list[0],
+					       can2_id_list[0],
+					       can2_id_list[0] };
+	can_add_filter(&can2, can2_id_list_size_four);
 	can_init(&can2);
 
 	can_outbound_queue =
