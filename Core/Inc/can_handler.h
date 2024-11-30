b1cd849e5fad9aad8a5e08a04b986617054f5401
bjackson312006 <blakejackson312006@gmail.com>
2024-11-30 00:19:09 -0500

    Changed fault timer can id

--- Core/Inc/can_handler.h
+++ Core/Inc/can_handler.h
@@ -25,7 +25,7 @@
 #define FAULT_CANID	    0x703
 #define NOISE_CANID	    0x88
 #define DEBUG_CANID	    0x702
-#define FAULT_TIMER_CANID   0x701
+#define FAULT_TIMER_CANID   0x6FF
 
 typedef struct {
 	uint32_t prev_tick;
