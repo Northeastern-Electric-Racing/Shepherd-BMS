5bc29c99ea0481748f24c3fbff60ec1c959928ca
Jake-Hensley <hensley.j@northeastern.edu>
2025-01-14 18:25:51 -0500

    added heading

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1185,6 +1185,34 @@ void watchdog_pet(void)
 
 }
 
+struct __attribute__((__packed__)) git_version_data {
+		uint8_t git_major_version;
+		uint8_t git_minor_version;
+		uint8_t git_patch_version;
+		bool git_is_upstream_clean;
+		bool git_is_local_clean;
+	} git_version_data;
+
+  struct __attribute__((__packed__)) git_hash_data {
+    uint32_t git_shorthash;
+    uint32_t git_authorhash;
+  } git_hash_data;
+
+//put funtion that makes can message and queues message, send_nero_message is similar, use variables from versioning over can ticket in embedded-base
+void send_git_version_message() {
+  const struct git_hash_data git_hash_data2 = {GIT_SHORTHASH , GIT_AUTHORHASH};
+  const struct git_version_data git_version_data2 = {GIT_MAJOR_VERSION , GIT_MINOR_VERSION, GIT_PATCH_VERSION, GIT_IS_UPSTREAM_CLEAN, GIT_IS_LOCAL_CLEAN};
+  can_msg_t msg1 = { .id = 0x698, .len = sizeof(git_version_data2)};
+  can_msg_t msg2 = { .id = 0x699, .len = sizeof(git_hash_data2)};
+
+  memcpy(&msg1.data, &git_version_data2, sizeof(git_version_data2));
+  memcpy(&msg2.data, &git_hash_data2, sizeof(git_hash_data2));
+
+  queue_can_msg(msg1);
+  //queue_can_msg(msg2);
+  
+}
+
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
