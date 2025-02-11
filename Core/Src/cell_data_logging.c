932fcb82e3a94ffb5ed9b52890ce5a258ef59847
Kaushik Mehta <mehta.kau@northeastern.edu>
2025-02-10 19:10:58 -0500

    Implemented function to log cell voltage and temperature data.

--- Core/Src/cell_data_logging.c
+++ Core/Src/cell_data_logging.c
@@ -23,3 +23,33 @@ void cell_data_logger_init(void)
 	rb_init(&cell_log_ring_buff, cell_data_storage, NUM_OF_READINGS,
 		sizeof(CellDataEntry_t));
 }
+
+void cell_data_log_measurement(acc_data_t *bms_data)
+{
+	if (bms_data == NULL) {
+		printf("No BMS data to log!!\r\n");
+		return;
+	}
+
+	CellDataEntry_t new_entry;
+
+	memset(&new_entry, 0, sizeof(CellDataEntry_t));
+
+	for (int chip_num = 0; chip_num < NUM_CHIPS; chip_num++) {
+		int cell_count = get_num_cells(&bms_data->chip_data[chip_num]);
+
+		for (int cell = 0; cell < cell_count; cell++) {
+			new_entry.cell_voltages[chip_num][cell] =
+				bms_data->chip_data[chip_num]
+					.cell_voltages[cell];
+
+			new_entry.cell_temperatures[chip_num][cell] =
+				bms_data->chip_data[chip_num].cell_temp[cell];
+
+			new_entry.timestamp[chip_num][cell] =
+				get_us_timestamp();
+		}
+	}
+
+	rb_insert(&cell_log_ring_buff, &new_entry);
+}
