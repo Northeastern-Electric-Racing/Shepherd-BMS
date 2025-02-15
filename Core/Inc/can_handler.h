f239ce517d7f479cb02a13ac3a76557a0f41613b
Jack Rubacha <rubacha.jack03@gmail.com>
2025-02-15 15:36:59 -0500

    fix another id

--- Core/Inc/can_handler.h
+++ Core/Inc/can_handler.h
@@ -77,7 +77,7 @@ typedef enum {
 } rate_lim_t;
 
 #define DEBUG_SIZE	  8
-#define FAULT_TIMER_CANID 0x6FF
+#define FAULT_TIMER_CANID 0x6F9
 #define FAULT_TIMER_SIZE  4
 
 void can_receive_callback(CAN_HandleTypeDef *hcan);
