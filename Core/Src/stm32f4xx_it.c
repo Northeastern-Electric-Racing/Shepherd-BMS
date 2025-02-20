b269d0d4f716f26785cc9c510bf6172f61e35efb
Jack Rubacha <rubacha.jack03@gmail.com>
2025-02-20 12:31:21 -0500

    initial 24a enablement, add LED code

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -97,6 +97,18 @@ void HardFault_Handler(void)
   while (1)
   {
     /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    volatile uint32_t i = 0;
+    while (i < 120000) {
+      i += 1;
+    }
+    HAL_GPIO_WritePin(DEBUG_LED_1_GPIO_Port, DEBUG_LED_1_Pin, 0);
+    HAL_GPIO_WritePin(DEBUG_LED_2_GPIO_Port, DEBUG_LED_2_Pin, 0);
+    i = 0;
+    while (i < 120000) {
+      i += 1;
+    }
+    HAL_GPIO_WritePin(DEBUG_LED_1_GPIO_Port, DEBUG_LED_1_Pin, 1);
+    HAL_GPIO_WritePin(DEBUG_LED_2_GPIO_Port, DEBUG_LED_2_Pin, 1);
     /* USER CODE END W1_HardFault_IRQn 0 */
   }
 }
