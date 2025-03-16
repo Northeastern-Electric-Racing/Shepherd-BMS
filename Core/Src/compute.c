8970e8f2c4b917cab03866733706fe58138e0d63
Daniel <dnakhooda@gmail.com>
2025-03-16 16:50:01 -0400

    fixes and documentation

--- Core/Src/compute.c
+++ Core/Src/compute.c
@@ -116,9 +116,9 @@ void compute_set_fault(int fault_state)
 
 float compute_get_pack_current()
 {
-	static const float CURRENT_ADC_RESOLUTION =
-		(5.0 / MAX_ADC_RESOLUTION) * (5.0 / 3.0);
-	static const float SENSOR_V_REF = 2.5;
+	static const float CURRENT_ADC_RESOLUTION = 3.3 / MAX_ADC_RESOLUTION;
+	static const float SENSOR_V_REF = 2.5; // From LA37S050S05KM Data Sheet
+	static const float VOLTAGE_DIVIDER = (3.0 / 5.0); // 2k and 3k resistors
 
 	// Get ADC reading
 	uint32_t adcValue;
@@ -126,7 +126,9 @@ float compute_get_pack_current()
 	       sizeof(channel_1_buf[0])); // From the rank of ADC_CHANNEL_15
 
 	// Convert ADC reading to volts and amps
-	float volts = ((float)adcValue * CURRENT_ADC_RESOLUTION) - SENSOR_V_REF;
+	float volts =
+		((float)adcValue * CURRENT_ADC_RESOLUTION / VOLTAGE_DIVIDER) -
+		SENSOR_V_REF;
 	float amps = volts / 0.0125; // Sensativity of 0.0125 Volts per Amp
 
 	return amps;
