4e728396110549b7ffce91d5d120f839dc06e457
KSMehta11 <mehta.kau@northeastern.edu>
2025-03-17 19:43:30 -0400

    Add: Fault logging and EEPROM read/write functions.

--- Core/Src/eepromdirectory.c
+++ Core/Src/eepromdirectory.c
@@ -1,158 +1,242 @@
 #include "eepromdirectory.h"
+#include "stm32f4xx_hal.h"
+#include "m24c32_eeprom_directory.h"
 #include "m24c32.h"
 
-void eepromInit()
+#define M24C32_DEVICE_ADDR (0x50U << 1)
+
+extern I2C_HandleTypeDef hi2c1;
+
+/**
+ * @brief Writes data to the EEPROM over I2C.
+ *
+ * @param address EEPROM memory address to write to.
+ * @param data Pointer to the data to be written.
+ * @param size Number of bytes to write.
+ * @return EEPROM_OK on success, EEPROM_ERROR_COMMS on communication failure.
+ */
+static eeprom_status_t I2C_write(uint16_t address, uint8_t *data, uint16_t size)
 {
-	eeprom_data[0].id = (char *)("ROOT");
-	eeprom_data[0].size = 1;
+	uint8_t buffer[2 + size];
+	buffer[0] = (address >> 8) & 0xFF;
+	buffer[1] = address & 0xFF;
+	memcpy(&buffer[2], data, size);
 
-	eeprom_data[1].id = (char *)("FAULTS");
-	eeprom_data[1].size = 21;
-
-	// Initialize EEPROM addresses given data and length
-
-	int i = 1;
-	int offset = 0;
-
-	/* initialize root offset to zero */
-	eeprom_data[0].address = EEPROM_ROOT_ADDR;
-
-	/* continue through array, setting offsets */ /* private funciton prototypes */
-	while (eeprom_data[i].id != NULL) {
-		offset += eeprom_data[i - 1].size;
-		eeprom_data[i].address = offset;
-		i++;
+	if (HAL_I2C_Master_Transmit(&hi2c1, M24C32_DEVICE_ADDR, buffer,
+				    size + 2, 1000) == HAL_OK) {
+		return EEPROM_OK;
 	}
-
-	// TODO ADD THIS BACK AND FIGURE OUT WHAT THE FUCK ITS TRYING TO DO
-	//Initialize first byte of faults partition to contain the address of the end of the partition so that the
-	//log_fault function logs the first fault to the first 4 bytes in the partition.
-	// eeprom_write_data_key((char*)("FAULTS"), (eeprom_data[eeprom_get_index((char*)("FAULTS"))]
-	// + eeprom_data[eeprom_get_index((char*)("FAULTS"))].size - 3), 1);
+	return EEPROM_ERROR_COMMS;
 }
 
-uint16_t eeprom_get_index(char *key)
+/**
+ * @brief Reads data from the EEPROM over I2C.
+ *
+ * @param address EEPROM memory address to read from.
+ * @param data Pointer to the buffer to store the read data.
+ * @param size Number of bytes to read.
+ * @return EEPROM_OK on success, EEPROM_ERROR_COMMS on communication failure.
+ */
+static eeprom_status_t I2C_read(uint16_t address, uint8_t *data, uint16_t size)
 {
-	/* find the index of the key in the eeprom_data array */
-	int i = 0;
-	while (eeprom_data[i].id != NULL) {
-		if (eeprom_data[i].id == key) {
-			return eeprom_data[i].address;
+	uint8_t addr[2] = { (address >> 8) & 0xFF, address & 0xFF };
+
+	if (HAL_I2C_Master_Transmit(&hi2c1, M24C32_DEVICE_ADDR, addr, 2,
+				    1000) != HAL_OK) {
+		return EEPROM_ERROR_COMMS;
+	}
+
+	if (HAL_I2C_Master_Receive(&hi2c1, M24C32_DEVICE_ADDR, data, size,
+				   1000) == HAL_OK) {
+		return EEPROM_OK;
+	}
+	return EEPROM_ERROR_COMMS;
+}
+
+m24c32_t eeprom_dev = { .write = I2C_write, .read = I2C_read };
+
+/**
+ * @brief Calculates a XOR checksum for a 32-bit fault code.
+ *
+ * @param fault_code The fault code to calculate checksum for.
+ * @return 8-bit checksum result.
+ */
+static uint8_t calculate_checksum(uint32_t fault_code)
+{
+	uint8_t *bytes = (uint8_t *)&fault_code;
+	uint8_t sum = 0;
+	for (int i = 0; i < 4; i++)
+		sum ^= bytes[i]; // XOR checksum
+	return sum;
+}
+
+eeprom_status_t eepromInit(eeprom_directory_t *directory,
+			   const struct partition_cfg *partitions,
+			   size_t num_partitions)
+{
+	eeprom_status_t status =
+		directory_init(directory, partitions, num_partitions);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	return EEPROM_OK;
+}
+
+eeprom_status_t eeprom_read_data_key(eeprom_directory_t *directory,
+				     const char *key, void *data, uint16_t size)
+{
+	eeprom_status_t status = m24c32_directory_read(directory, &eeprom_dev,
+						       key, data, size, 0);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	return EEPROM_OK;
+}
+
+eeprom_status_t eeprom_read_data_address(eeprom_directory_t *directory,
+					 const char *key, uint16_t address,
+					 void *data, uint16_t size)
+{
+	uint16_t base;
+	eeprom_status_t status = eeprom_get_base_address(directory, key, &base);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	if (address < base) {
+		return EEPROM_ERROR_OUT_OF_BOUNDS;
+	}
+
+	status = m24c32_directory_read(directory, &eeprom_dev, key, data, size,
+				       address - base);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	return EEPROM_OK;
+}
+
+eeprom_status_t eeprom_write_data_key(eeprom_directory_t *directory,
+				      const char *key, void *data,
+				      uint16_t size)
+{
+	eeprom_status_t status = m24c32_directory_write(
+		directory, &eeprom_dev, key, (uint8_t *)data, size, 0);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	return EEPROM_OK;
+}
+
+eeprom_status_t eeprom_write_data_address(eeprom_directory_t *directory,
+					  const char *key, uint16_t address,
+					  void *data, uint16_t size)
+{
+	uint16_t base;
+	eeprom_status_t status = eeprom_get_base_address(directory, key, &base);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	if (address < base) {
+		return EEPROM_ERROR_OUT_OF_BOUNDS;
+	}
+
+	status = m24c32_directory_write(directory, &eeprom_dev, key,
+					(uint8_t *)data, size, address - base);
+
+	if (status != EEPROM_OK)
+		return status;
+
+	return EEPROM_OK;
+}
+
+eeprom_status_t log_fault(eeprom_directory_t *directory, uint32_t fault_code)
+{
+	uint8_t fault_index = 0;
+	eeprom_status_t status;
+
+	status = m24c32_directory_read(directory, &eeprom_dev, "FAULTS",
+				       &fault_index, 1, 0);
+	if (status != EEPROM_OK)
+		return status;
+
+	uint16_t partition_size;
+	status = eeprom_get_size(directory, "FAULTS", &partition_size);
+	if (status != EEPROM_OK)
+		return status;
+
+	uint8_t max_faults = (partition_size - 1) / 5;
+
+	if (fault_index >= max_faults)
+		fault_index = 0;
+
+	uint16_t relative_offset = 1 + (fault_index * 5);
+
+	uint8_t fault_packet[5];
+	memcpy(fault_packet, &fault_code, 4);
+	fault_packet[4] = calculate_checksum(fault_code);
+
+	status = m24c32_directory_write(directory, &eeprom_dev, "FAULTS",
+					fault_packet, 5, relative_offset);
+	if (status != EEPROM_OK)
+		return status;
+
+	fault_index = (fault_index + 1) % max_faults;
+	return m24c32_directory_write(directory, &eeprom_dev, "FAULTS",
+				      &fault_index, 1, 0);
+}
+
+eeprom_status_t get_faults(eeprom_directory_t *directory, uint32_t *faults,
+			   uint8_t n)
+{
+	uint8_t fault_index = 0;
+	eeprom_status_t status;
+
+	status = m24c32_directory_read(directory, &eeprom_dev, "FAULTS",
+				       &fault_index, 1, 0);
+	if (status != EEPROM_OK)
+		return status;
+
+	uint16_t partition_size;
+	status = eeprom_get_size(directory, "FAULTS", &partition_size);
+	if (status != EEPROM_OK)
+		return status;
+
+	uint8_t max_faults = (partition_size - 1) / 5;
+
+	if (n > max_faults)
+		return EEPROM_ERROR_OUT_OF_BOUNDS;
+
+	int start_index = (fault_index == 0) ? (max_faults - 1) :
+					       (fault_index - 1);
+
+	int valid_fault_count = 0;
+
+	for (int i = 0; i < n; i++) {
+		uint8_t fault_packet[5];
+		uint16_t relative_offset = 1 + (start_index * 5);
+
+		status = m24c32_directory_read(directory, &eeprom_dev, "FAULTS",
+					       fault_packet, 5,
+					       relative_offset);
+		if (status != EEPROM_OK)
+			return status;
+
+		uint32_t fault;
+		memcpy(&fault, fault_packet, 4);
+		uint8_t checksum = fault_packet[4];
+
+		if (calculate_checksum(fault) == checksum) {
+			faults[valid_fault_count++] = fault;
 		}
 
-		i++;
-	}
-	return -1;
-}
-
-char *eeprom_get_key(int index)
-{
-	/* find the key at the index in the eeprom_data array */
-	int i = 0;
-	while (eeprom_data[i].id != NULL) {
-		if (eeprom_data[i].address == index) {
-			return eeprom_data[i].id;
-		}
-
-		i++;
-	}
-	return NULL;
-}
-
-bool eeprom_read_data_key(char *key, void *data, uint16_t size)
-{
-	if (!data) {
-		return false;
-	}
-	/* read data from eeprom given key and size */
-	int address = eeprom_get_index(key);
-	eeprom_read(address, data, size);
-
-	return true;
-}
-
-bool eeprom_read_data_address(uint16_t address, void *data, uint16_t size)
-{
-	if (!data) {
-		return false;
-	}
-	/* read data from eeprom given index */
-	eeprom_read(address, data, size);
-	return true;
-}
-
-bool eeprom_write_data_key(char *key, void *data, uint16_t size)
-{
-	if (!data) {
-		return false;
-	}
-	/* write data to eeprom given key, offset, and size of data */
-	int address = eeprom_get_index(key);
-	eeprom_write(address, data, size);
-	return true;
-}
-
-bool eeprom_write_data_address(uint16_t address, void *data, uint16_t size)
-{
-	if (!data) {
-		return false;
-	}
-	/* write data to eeprom given page, offset, and size of data */
-	eeprom_write(address, data, size);
-	return true;
-}
-
-void log_fault(uint32_t fault_code)
-{
-	uint32_t fault = fault_code;
-	//The next address to write a fault to is located in the first byte of the FAULTS partition.
-	uint8_t reg_to_write;
-	eeprom_read_data_address(eeprom_get_index((char *)("FAULTS")),
-				 &reg_to_write, 1);
-
-	uint8_t startIndex =
-		eeprom_data[eeprom_get_index((char *)("FAULTS"))].address;
-	uint8_t size = eeprom_data[eeprom_get_index((char *)("FAULTS"))].size;
-
-	/* if the index is at the end of the partition, wrap around (currently store 5 faults, so max = 5 + offset) */
-	if (reg_to_write == size + startIndex - 3) {
-		/* first byte of partition is the index of the most recent fault, faults begin at second byte */
-		reg_to_write = startIndex + 1;
-	} else {
-		reg_to_write += 4;
+		start_index = (start_index == 0) ? (max_faults - 1) :
+						   (start_index - 1);
 	}
 
-	/* write the fault code*/
-	eeprom_write_data_address(reg_to_write, &fault, 4);
-	/* update first byte of faults partition*/
-	eeprom_write_data_address(startIndex, &reg_to_write, 1);
-}
-
-void get_faults()
-{
-	uint8_t curr_reg;
-	eeprom_read_data_key("FAULTS", &curr_reg, 1);
-
-	uint8_t startAddress =
-		eeprom_data[eeprom_get_index((char *)("FAULTS"))].address;
-	uint8_t size = eeprom_data[eeprom_get_index((char *)("FAULTS"))].size;
-
-	/* read and store the faults */
-
-	int currIter = 0;
-	while (currIter < NUM_EEPROM_FAULTS) {
-		eeprom_read_data_address(curr_reg, &eeprom_faults[currIter], 4);
-		currIter++;
-
-		/* if the index is at the end of the partition, wrap around (5 faults * 4 bytes per fault + offset - 3  for start of fault) */
-		if (curr_reg == size + startAddress - 3) {
-			/* first byte of partition is the index of the most recent fault, faults begin at second byte */
-			curr_reg = startAddress + 1;
-		}
-
-		else {
-			/* 4 bytes per fault */
-			curr_reg += 4;
-		}
-	}
+	return EEPROM_OK;
 }
