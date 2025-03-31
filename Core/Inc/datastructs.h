ab1d43bb8d6b1620a52e5802cf2899a1720a2834
Blake Jackson <blakejackson312006@gmail.com>
2025-03-30 22:59:06 -0500

    Charging Detection 24A (#211)

--- Core/Inc/datastructs.h
+++ Core/Inc/datastructs.h
@@ -155,6 +155,7 @@ typedef struct {
 	uint16_t boost_setting;
 
 	bool is_charger_connected;
+	bool is_charging_enabled;
 
 	osMutexId_t mutex;
 } acc_data_t;
--- Core/Src/can_messages.c
+++ Core/Src/can_messages.c
@@ -14,8 +14,6 @@
 #define CELL_ID_BITS 4
 #define VA_VD_BITS   10 /* Vanalog and Vdigital internal references */
 
-extern is_charging_enabled;
-
 static unsigned short reverse_short(unsigned short val);
 
 static unsigned short reverse_short(unsigned short val)
@@ -74,7 +72,7 @@ int send_charging_message(uint16_t voltage_to_set, uint16_t current_to_set,
 	charger_msg_data.charger_voltage = voltage_to_set * 10;
 	charger_msg_data.charger_current = current_to_set * 10;
 
-	if (is_charging_enabled) {
+	if (bms_data->is_charging_enabled) {
 		charger_msg_data.charger_control = 0x00; // 0：Start charging.
 	} else {
 		charger_msg_data.charger_control =
@@ -84,7 +82,7 @@ int send_charging_message(uint16_t voltage_to_set, uint16_t current_to_set,
 	charger_msg_data.reserved_1 = 0x00;
 	charger_msg_data.reserved_23 = 0x0000;
 
-	can_msg_t charger_msg = { .id = 0x1806E5F4,
+	can_msg_t charger_msg = { .id = CHARGER_CANID,
 				  .id_is_extended = true,
 				  .len = 8,
 				  .data = { 0 } };
@@ -97,12 +95,12 @@ int send_charging_message(uint16_t voltage_to_set, uint16_t current_to_set,
 	charger_msg.data[2] = charger_msg.data[3];
 	charger_msg.data[3] = temp;
 
-#ifdef CHARGING_ENABLED
-	HAL_StatusTypeDef res = can_send_msg(&can2, &msg);
-	if (res != HAL_OK) {
-		printf("CAN ERROR CODE %X", res);
+	if (bms_data->is_charging_enabled) {
+		HAL_StatusTypeDef res = queue_can_msg(charger_msg);
+		if (res != HAL_OK) {
+			printf("queue_can_msg() ERROR CODE %X", res);
+		}
 	}
-#endif
 
 	return 0;
 }
