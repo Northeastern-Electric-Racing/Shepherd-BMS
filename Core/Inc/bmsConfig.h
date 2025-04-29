52e05505f7462018be1faf2b0736f7f321fbdefc
Caio <caiodasilva05@outlook.com>
2025-04-29 19:47:00 -0400

    charging

--- Core/Inc/bmsConfig.h
+++ Core/Inc/bmsConfig.h
@@ -56,7 +56,7 @@
 #define OCV_CURR_THRESH 3 /* in A */
 
 // Charging settings
-#define CHARGING_CURRENT    0
+#define CHARGING_CURRENT    3.5
 #define CHARGE_SETL_TIMEOUT 60000 // 1 minute, may need adjustment
 #define CHARGE_SETL_TIMEUP  300000 // 5 minutes, may need adjustment
 
