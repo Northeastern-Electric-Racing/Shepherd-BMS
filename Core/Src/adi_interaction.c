dc3887d2a98a1448d44b2bc7d3667703fe104fdd
Jack Rubacha <rubacha.jack03@gmail.com>
2025-05-13 16:51:35 -0400

    pwm go hard

--- Core/Src/adi_interaction.c
+++ Core/Src/adi_interaction.c
@@ -86,6 +86,14 @@ void set_cell_discharge(cell_asic *chip, DCC cell, DCC_BIT discharge)
 	}
 }
 
+void set_cell_pwm(cell_asic *chip, DCC cell, PWM_DUTY discharge)
+{
+	if (cell < 12)
+		chip->PwmA.pwma[cell] = discharge;
+	else
+		chip->PwmB.pwmb[cell] = discharge;
+}
+
 void clear_cell_discharge(cell_asic *chip)
 {
 	chip->tx_cfgb.dcc = 0;
@@ -272,13 +280,18 @@ void snap_chips(cell_asic chips[NUM_CHIPS])
 void unsnap_chips(cell_asic chips[NUM_CHIPS])
 {
 	adbms_wake_isospi();
-	spiSendCmd(MUTE);
+	spiSendCmd(UNSNAP);
 }
 
 void write_config_regs(cell_asic chips[NUM_CHIPS])
 {
 	write_adbms_data(chips, WRCFGA, Config, A);
 	write_adbms_data(chips, WRCFGB, Config, B);
+}
+void write_pwm_regs(cell_asic chips[NUM_CHIPS])
+{
+	write_adbms_data(chips, WRPWM1, Pwm, A);
+	write_adbms_data(chips, WRPWM2, Pwm, B);
 }
 
 void write_clear_flags(cell_asic chips[NUM_CHIPS])
