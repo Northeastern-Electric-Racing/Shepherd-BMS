a1cfc0348c199dcf3631869059254c265076e724
Scott A <89099102+Sabramz@users.noreply.github.com>
2025-05-26 18:19:51 -0400

    fix balancing. Can't believe we're getting another scott-charging branch

--- Core/Src/charging.c
+++ Core/Src/charging.c
@@ -13,90 +13,41 @@
 		_a < _b ? _a : _b;      \
 	})
 
-/// @brief A struct to hold the original float value and the index originally, as that holds meaning
-typedef struct {
-	float val;
-	size_t idex;
-} val_idexed_t;
-
-/**
- * @brief selection sorts ocv into structs that remember values
- * @param arr 
- * @param n count
- */
-void chipsSelectionSort(acc_data_t *bmsdata,
-			val_idexed_t replaced_val[NUM_CHIPS][NUM_CELLS_ALPHA])
-{
-	for (size_t chip = 0; chip < NUM_CHIPS; chip++) {
-		uint8_t cells = get_num_cells(bmsdata[chip].chip_data);
-		// first fill the outer row
-		for (int i = 0; i < cells; i++) {
-			replaced_val[chip][i] = (val_idexed_t){
-				.idex = i,
-				.val = bmsdata->chip_data[chip]
-					       .open_cell_voltage[i]
-			};
-		}
-
-		// now actually sort it
-		for (size_t i = 0; i < cells - 1; i++) {
-			// Assume the current position holds
-			// the minimum element
-			size_t max_idx = i;
-
-			// Iterate through the unsorted portion
-			// to find the actual minimum
-			for (size_t j = i + 1; j < cells; j++) {
-				if (replaced_val[chip][j].val >
-				    replaced_val[chip][max_idx].val) {
-					// Update min_idx if a smaller element is found
-					max_idx = j;
-				}
-			}
-
-			// Move minimum element to its
-			// correct position
-			val_idexed_t temp = replaced_val[chip][i];
-			replaced_val[chip][i] = replaced_val[chip][max_idx];
-			replaced_val[chip][max_idx] = temp;
-		}
-	}
-}
-
 /* Send cell balancing config to the segments */
 void handle_balance_cells(acc_data_t *bmsdata)
 {
-	// the maximum number of cells to balance per chip, usually tuned for thermal reasons
-	static const int MAX_BAL_CHIP = 7;
+	/* 
+	 * The maximum number of cells to balance per chip, usually tuned
+	 * for thermal reasons. With the lid off and cooling fans at maximum 
+	 * power, we can sustain balancing 8 cells per chip.
+	 */
+	static const int MAX_BAL_CHIP = 8;
 
-	// the low cell, eventually they all must get there
-	float low = bmsdata->min_ocv.val;
-	// the margin above the low cell to ignore, which is usually X% of the delta
-	float min_thresh = bmsdata->delt_ocv * 0.4;
+	/*
+	 * A cell's voltage must be greater than this plus the low cell to balance. 
+	 * This value is usually proportional to the delta.
+	 */
+	const float min_thresh = 0.010;
 
-	val_idexed_t new_ocv_map[NUM_CHIPS][NUM_CELLS_ALPHA] = { 0 };
-
-	// first, sort and cleanup everything
-	chipsSelectionSort(bmsdata, new_ocv_map);
-
-	/* Balance all cells above the threshold, using the sorted ocv map values but preserve the indexes*/
 	for (size_t chip = 0; chip < NUM_CHIPS; chip++) {
-		// ONLY iterate to MAX_BAL or the number of cells, whatever is lower.
-		// this is OK because they are sorted greatest to least in delta
-		int cell_max = min(get_num_cells(bmsdata[chip].chip_data),
-				   MAX_BAL_CHIP);
-		for (size_t cell = 0; cell < cell_max; cell++) {
-			/* Check if cell voltage is above (low + threshold) */
-			if (new_ocv_map[chip][cell].val > (low + min_thresh)) {
-				/* Balance cell */
-				bmsdata->discharge_config
-					[chip][new_ocv_map[chip][cell].idex] =
-					true;
-			} else {
-				/* Do not balance cell */
-				bmsdata->discharge_config
-					[chip][new_ocv_map[chip][cell].idex] =
-					false;
+		/* Number of cells in this chip. */
+		uint8_t num_cells = get_num_cells(&bmsdata->chip_data[chip]);
+
+		/* Clear balancing config. */
+		for (uint8_t cell = 0; cell < num_cells; cell++) {
+			bmsdata->discharge_config[chip][cell] = false;
+		}
+
+		/* Number of cells that can be balanced. */
+		int cells_left = MAX_BAL_CHIP;
+
+		/* Balance cells with an OCV above the threshold. */
+		for (size_t cell = 0; (cell < num_cells) && (cells_left > 0);
+		     cell++) {
+			if (bmsdata->chip_data[chip].open_cell_voltage[cell] >
+			    (bmsdata->min_ocv.val + min_thresh)) {
+				bmsdata->discharge_config[chip][cell] = true;
+				cells_left -= 1;
 			}
 		}
 	}
